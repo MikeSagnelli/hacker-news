fe17e97695857d9bf1ce1dc44dd8183e95fb3377
MikeSagnelli <ing.sagnelli@gmail.com>
2024-07-20 20:54:58 +0200

    chore: setup libraries

---
+++ libs/ui/hooks/src/lib/ui-hooks.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledUiHooks = styled.div`
+  color: pink;
+`;
+
+export function UiHooks() {
+  return (
+    <StyledUiHooks>
+      <h1>Welcome to UiHooks!</h1>
+    </StyledUiHooks>
+  );
+}
+
+export default UiHooks;
---
+++ libs/ui/utils/src/lib/ui-utils.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledUiUtils = styled.div`
+  color: pink;
+`;
+
+export function UiUtils() {
+  return (
+    <StyledUiUtils>
+      <h1>Welcome to UiUtils!</h1>
+    </StyledUiUtils>
+  );
+}
+
+export default UiUtils;
